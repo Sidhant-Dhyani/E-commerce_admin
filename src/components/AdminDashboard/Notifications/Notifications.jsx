782b22b1b6a398d4a1b33327883c2e64de0911b5
sidhantdhyani <siddhantydhyani99@gmail.com>
2023-12-15 18:16:18 +0530

    notifications left, everything complete

--- src/components/AdminDashboard/Notifications/Notifications.jsx
+++ src/components/AdminDashboard/Notifications/Notifications.jsx
@@ -1,7 +1,20 @@
 
 import React from 'react';
+import socket from '../../../socket';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Notifications = () => {
+    const [msg, setMsg] = useState({});
+    useEffect(() => {
+        socket.on('notification', (message) => {
+            console.log('Notification recieved:', message);
+        });
+        return () => {
+            socket.off('notification');
+        }
+    }, [])
+
     return (
         <div>
             <h2>Notifications</h2>
